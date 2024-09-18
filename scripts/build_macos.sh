b32dfa3d582ac3736d78c8786f7f43953fa1ba41
Kostiantyn Shyrolapov <kostya.shirolapov@gmail.com>
2024-09-18 00:20:54 -0400

    ci: macos signing(19)

--- scripts/build_macos.sh
+++ scripts/build_macos.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
-
 export PATH=${PATH}:`go env GOPATH`/bin
 
-# Function to build for a specific platform
 build_for_platform() {
     local platform=$1
     local output_suffix=$2
@@ -14,23 +12,15 @@ build_for_platform() {
     gon -log-level=info ./build/darwin/gon-sign.json
 
     echo "Zipping Package"
-    ditto -c -k --keepParent ./build/bin/flower.app ../bin/flower_$output_suffix.zip
-
-    echo "Cleaning up"
-    rm -rf ./build/bin/flower.app
+    appdmg ./build/appdmg.json ../bin/flower_$output_suffix.dmg
 }
 
-# Ensure we're in the desktop directory
 cd "$(dirname "$0")/../desktop" || exit
 
-# Create bin directory if it doesn't exist
 mkdir -p ../bin
 
-# Build for AMD64
-build_for_platform "darwin/amd64" "darwin-amd64"
+build_for_platform "darwin/amd64" "amd64"
+build_for_platform "darwin/arm64" "arm64"
 
-# Build for ARM64
-build_for_platform "darwin/arm64" "darwin-arm64"
-
-echo "Notarizing Zip Files"
-gon -log-level=info ./build/darwin/gon-notarize.json
+#echo "Notarizing Dmg?"
+#gon -log-level=info ./build/darwin/gon-notarize.json
